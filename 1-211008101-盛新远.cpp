b5208b7e11da1a4f2ef33feadf791b90fbc428db
STSDXY <2479998100@qq.com>
2024-04-01 15:34:20 +0800

    盛新远

---
+++ 1-211008101-盛新远.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <cmath>
+#define shengxinyuan 3.14
+using namespace std;
+
+void pingyixuanzhuan(double* x, double* y, double a, double b, double theta) {
+	double c = *x;
+	double d = *y;
+	double e, f;
+	e = cos(theta) * (*x) - sin(theta) * (*y) - a;
+	f = sin(theta) * (*x) + cos(theta) * (*y) - b;
+	*x = e;
+	*y = f;
+}
+
+int main() {
+	double a = 10;
+	double b = 25;
+	double x = 10;
+	double y = 10;
+	double x1 = 30;
+	double y1 = 10;
+	double x2 = 20;
+	double y2 = 25;
+	double theta=shengxinyuan;
+	theta = theta / 6;
+	pingyixuanzhuan(&x, &y, a, b, theta);
+	pingyixuanzhuan(&x1, &y1, a, b, theta);
+	pingyixuanzhuan(&x2, &y2, a, b, theta);
+	cout << "Point1:(" << x<<","<<y<< ")" << endl;
+	cout << "Point2:(" << x1 << "," << y1 << ")" << endl;
+	cout << "Point3:(" << x2 << "," << y2 << ")" << endl;
+	return 0;
+}
---
+++ 2-211008101-盛新远.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <cmath>
+#define shengxinyuan 3.14
+using namespace std;
+void shuzuhanshu(double* x, double* y, double k, double a, double b, double* resultx, double* resulty) {
+	double c;
+	double d;
+	c = atan(k);
+	d = shengxinyuan / 2 - c;
+	for (int i = 0; i < 3; i++) {
+		resultx[i] = cos(d) * x[i] + sin(d) * y[i] + cos(d) * a + sin(d) * b - a;
+		resulty[i] = -sin(d) * x[i] + cos(d) * y[i] - sin(d) * a + cos(d) * b - b;
+	}
+}
+int main() {
+	double x[3] = { 10, 30, 20 };
+	double y[3] = { 10, 10, 25 };
+	double a = 0;
+	double b = 1;
+	double k = 2;
+	double resultx[3] = { 0 };
+	double resulty[3] = { 0 };
+	shuzuhanshu(x, y, k, a, b, resultx, resulty);
+	for (int i = 0; i < 3; i++) {
+		cout << "P" <<i<<"(" <<resultx[i]<<","<<resulty[i]<<")" << endl;
+	}
+	return 0;
+}
